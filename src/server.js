aeefcd6dc9fe128103e33313770b523fa2367888
joyceeazevedo <azevedoeferreira@gmail.com>
2019-12-11 19:22:59 -0300

    novoteste

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require("./app.js")
-const port = process.env.Port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, function() {
   console.log(`app está rodando na porta ${port}`)
